ace20f7236d3fced9d32b4735a08e8a403bf3bd9
Piotr <pskirski@hotmail.com>
2024-05-20 21:56:07 +0200

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,14 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Gallery from "./components/Gallery";
 import Home from "./components/Home";
-import Menu from "./components/Menu";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <div className="site-wrap">
-        <div className="site-navbar mt-4">
-          <div className="container py-1">
-            <div className="row align-items-center">
-              <div className="col-8 col-md-8 col-lg-4">
-                <h1 className="mb-0">
-                  <a href="index.html" className="text-white h2 mb-0">
-                    <strong>
-                      Wakacje 4U<span className="text-primary">.</span>
-                    </strong>
-                  </a>
-                </h1>
-              </div>
-              <div className="col-4 col-md-4 col-lg-8">
-                <nav
-                  className="site-navigation text-right text-md-right"
-                  role="navigation"
-                >
-                  <div className="d-inline-block d-lg-none ml-md-0 mr-auto py-3">
-                    <a
-                      href="#"
-                      className="site-menu-toggle js-menu-toggle text-white"
-                    >
-                      <span className="icon-menu h3"></span>
-                    </a>
-                  </div>
-
-                  <Menu />
-                </nav>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="site-mobile-menu">
-        <div className="site-mobile-menu-header">
-          <div className="site-mobile-menu-close mt-3">
-            <span className="icon-close2 js-menu-toggle"></span>
-          </div>
-        </div>
-        <div className="site-mobile-menu-body"></div>
-      </div>
+      <Header />
 
       <Routes>
         <Route path="/" Component={Home} />
@@ -57,147 +16,7 @@ function App() {
         <Route path="/contact" Component={Contact} />
       </Routes>
 
-      <footer className="site-footer">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4">
-              <div className="mb-5">
-                <h3 className="footer-heading mb-4">About Apart</h3>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Saepe pariatur reprehenderit vero atque, consequatur id
-                  ratione, et non dignissimos culpa? Ut veritatis, quos illum
-                  totam quis blanditiis, minima minus odio!
-                </p>
-              </div>
-              <div className="mb-5">
-                <h3 className="footer-heading mb-4">Subscribe</h3>
-                <form action="#" method="post" className="site-block-subscribe">
-                  <div className="input-group mb-3">
-                    <input
-                      type="text"
-                      className="form-control border-secondary bg-transparent"
-                      placeholder="Enter your email"
-                      aria-label="Enter Email"
-                      aria-describedby="button-addon2"
-                    />
-                    <div className="input-group-append">
-                      <button
-                        className="btn btn-primary rounded-top-right-0"
-                        type="button"
-                        id="button-addon2"
-                      >
-                        Subscribe
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-            <div className="col-lg-4 mb-5 mb-lg-0">
-              <div className="row mb-5">
-                <div className="col-md-12">
-                  <h3 className="footer-heading mb-4">Navigations</h3>
-                </div>
-                <div className="col-md-6 col-lg-6">
-                  <ul className="list-unstyled">
-                    <li>
-                      <a href="#">Home</a>
-                    </li>
-                    <li>
-                      <a href="#">Apartments</a>
-                    </li>
-                    <li>
-                      <a href="#">News</a>
-                    </li>
-                    <li>
-                      <a href="#">Featured Apartment</a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="col-md-6 col-lg-6">
-                  <ul className="list-unstyled">
-                    <li>
-                      <a href="#">About Us</a>
-                    </li>
-                    <li>
-                      <a href="#">Privacy Policy</a>
-                    </li>
-                    <li>
-                      <a href="#">Contact Us</a>
-                    </li>
-                    <li>
-                      <a href="#">Membership</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              <div className="row">
-                <div className="col-md-12">
-                  <h3 className="footer-heading mb-4">Follow Us</h3>
-
-                  <div>
-                    <a href="#" className="pl-0 pr-3">
-                      <span className="icon-facebook"></span>
-                    </a>
-                    <a href="#" className="pl-3 pr-3">
-                      <span className="icon-twitter"></span>
-                    </a>
-                    <a href="#" className="pl-3 pr-3">
-                      <span className="icon-instagram"></span>
-                    </a>
-                    <a href="#" className="pl-3 pr-3">
-                      <span className="icon-linkedin"></span>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 mb-5 mb-lg-0">
-              <div className="mb-5">
-                <h3 className="footer-heading mb-4">Watch Live Streaming</h3>
-
-                <div className="block-16">
-                  <figure>
-                    <img
-                      src="images/img_1.jpg"
-                      alt="Image placeholder"
-                      className="img-fluid rounded"
-                    />
-                    <a
-                      href="https://vimeo.com/channels/staffpicks/93951774"
-                      className="play-button popup-vimeo"
-                    >
-                      <span className="icon-play"></span>
-                    </a>
-                  </figure>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row pt-5 mt-5 text-center">
-            <div className="col-md-12">
-              <p>
-                {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
-                Copyright &copy;
-                <script
-                  data-cfasync="false"
-                  src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"
-                ></script>
-                <script>document.write(new Date().getFullYear());</script> All
-                rights reserved | This template is made with{" "}
-                <i className="icon-heart text-danger" aria-hidden="true"></i> by{" "}
-                <a href="https://colorlib.com" target="_blank">
-                  Colorlib
-                </a>
-                {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
-              </p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import Menu from "./Menu";
+
+const Header = () => {
+  return (
+    <>
+      <div className="site-wrap">
+        <div className="site-navbar mt-4">
+          <div className="container py-1">
+            <div className="row align-items-center">
+              <div className="col-8 col-md-8 col-lg-4">
+                <h1 className="mb-0">
+                  <Link to="/" className="text-white h2 mb-0">
+                    <strong>
+                      Wakacje 4U<span className="text-primary">.</span>
+                    </strong>
+                  </Link>
+                </h1>
+              </div>
+              <div className="col-4 col-md-4 col-lg-8">
+                <nav
+                  className="site-navigation text-right text-md-right"
+                  role="navigation"
+                >
+                  <div className="d-inline-block d-lg-none ml-md-0 mr-auto py-3">
+                    <a
+                      href="#"
+                      className="site-menu-toggle js-menu-toggle text-white"
+                    >
+                      <span className="icon-menu h3"></span>
+                    </a>
+                  </div>
+
+                  <Menu />
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="site-mobile-menu">
+        <div className="site-mobile-menu-header">
+          <div className="site-mobile-menu-close mt-3">
+            <span className="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div className="site-mobile-menu-body"></div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
