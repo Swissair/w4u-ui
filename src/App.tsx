b4829c247883d3bf1f249dc7930138cbb8d15cf2
Piotr <pskirski@hotmail.com>
2024-05-20 20:42:41 +0200

    Fixed styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
-// import { indexImage } from "../public/images/hero_bg_1.jpg";
 
 function App() {
   return (
